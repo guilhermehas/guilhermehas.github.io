5bb987deaecf359535d6333e20aa5430fc0d80d6
Guilherme <guilhermehas@hotmail.com>
2021-05-20 17:26:37 -0300

    cabal updated

---
+++ agda-posts/example.lagda.md
@@ -0,0 +1,12 @@
+---
+date: 2021-05-20
+title: Writing Agda blog posts
+author: Guilherme
+---
+
+My first agda Post
+
+```
+data MyData : Set where
+  myConst : MyData
+```
--- site.hs
+++ site.hs
@@ -1,7 +1,24 @@
 --------------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings #-}
-import           Data.Monoid (mappend)
+import           Control.Monad
+-- import           Control.Monad.Extra
+
+import           Data.List
+import           Data.Monoid
+import           Data.String
+
 import           Hakyll
+import           Hakyll.Core.UnixFilter
+import           Hakyll.Web.Pandoc.Biblio
+
+-- import           System.Directory
+import           System.Exit
+-- import           System.FilePath
+-- import           System.Process
+
+-- import           Text.Pandoc.Options
+
+import qualified GHC.IO.Encoding as E
 
 
 --------------------------------------------------------------------------------
@@ -64,3 +81,40 @@ postCtx :: Context String
 postCtx =
     dateField "date" "%B %e, %Y" `mappend`
     defaultContext
+
+--------------------------------------------------------------------------------
+
+-- agdaCommand :: String
+-- agdaCommand = "agda"
+
+-- agdaInputDir :: String
+-- agdaInputDir = "agda-posts"
+
+-- agdaOutputDir :: String
+-- agdaOutputDir = "_agda"
+
+-- agdaOptions :: String -> [String]
+-- agdaOptions fileName =
+--   [ "--html"
+--   , "--html-highlight=auto"
+--   , "--html-dir=" ++ agdaOutputDir
+--   , "-i" ++ agdaInputDir
+--   , agdaInputDir </> fileName
+--   ]
+
+-- -- Process a .lagda.md file into markdown by calling Agda
+-- processAgdaPosts :: IO ()
+-- processAgdaPosts = do
+--   files <- listDirectory agdaInputDir
+--   let agdaFiles = filter (".lagda.md" `isSuffixOf`) files
+--   forM_ agdaFiles processAgdaPost
+
+-- processAgdaPost :: FilePath -> IO ()
+-- processAgdaPost agdaFile = do
+--   exitCode <- readProcessWithExitCode agdaCommand (agdaOptions agdaFile) mempty
+--   case exitCode of
+--     (ExitFailure _ , err , _) -> do
+--       putStrLn $ "Failed to process " ++ agdaFile
+--       putStrLn err
+--     (ExitSuccess   , out , _) -> do
+--       putStrLn out
